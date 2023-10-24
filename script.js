9c344fcec7f758abb2e359837f39018e58021291
Yorften <144786380+Yorften@users.noreply.github.com>
2023-10-24 16:35:42 +0100

    done

--- script.js
+++ script.js
@@ -1,45 +1,67 @@
 const radioInputs = document.querySelectorAll('input[type="radio"]');
 
-
 function changeActiveItem(nextIndex) {
+  radioInputs.forEach((input) => (input.checked = false));
 
-    radioInputs.forEach(input => input.checked = false);
-
-    radioInputs[nextIndex].checked = true;
+  radioInputs[nextIndex].checked = true;
 }
 
-
 function autoLoopCarousel() {
+  const activeItem = document.querySelector('input[type="radio"]:checked');
 
-    const activeItem = document.querySelector('input[type="radio"]:checked');
+  const currentIndex = Array.from(radioInputs).indexOf(activeItem);
 
-    const currentIndex = Array.from(radioInputs).indexOf(activeItem);
+  const nextIndex = (currentIndex + 1) % radioInputs.length;
 
-
-    const nextIndex = (currentIndex + 1) % radioInputs.length;
-
-
-    changeActiveItem(nextIndex);
+  changeActiveItem(nextIndex);
 }
 
 setInterval(autoLoopCarousel, 4000);
 
-
 let isRotated = false;
 
 function rotateButton(button) {
-    isRotated = !isRotated;
+  isRotated = !isRotated;
 
-    if (isRotated) {
-        button.style.transform = 'rotate(180deg)';
-    } else {
-        button.style.transform = 'rotate(0deg)';
-    }
+  if (isRotated) {
+    button.style.transform = "rotate(180deg)";
+  } else {
+    button.style.transform = "rotate(0deg)";
+  }
 
-    const panel = button.parentElement.nextElementSibling;
-    if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-    } else {
-        panel.style.maxHeight = panel.scrollHeight + 'px';
-    }
+  const panel = button.parentElement.nextElementSibling;
+  if (panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+  } else {
+    panel.style.maxHeight = panel.scrollHeight + "px";
+  }
+}
+
+const loadMoreButton = document.getElementById("loadMoreButton");
+const loadLessButton = document.getElementById("loadLessButton");
+const rowsToShow = document.querySelectorAll(".galerie-container.show");
+
+let isShowingAll = false;
+
+function loadMore() {
+  rowsToShow.forEach((row) => {
+    row.style.maxHeight = "684px"; // Adjust the value for desired max height
+  });
+  loadMoreButton.style.display = "none";
+  loadLessButton.style.display = "block";
+  isShowingAll = true;
+}
+
+function loadLess() {
+  rowsToShow.forEach((row) => {
+    row.style.maxHeight = "0";
+  });
+  loadMoreButton.style.display = "block";
+  loadLessButton.style.display = "none";
+  isShowingAll = false;
+}
+
+if (rowsToShow.length === 0) {
+  loadMoreButton.style.display = "none";
+  loadLessButton.style.display = "none";
 }
